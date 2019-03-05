f9aeb417349cd6d117a95c0f95ed74ef717c57a1
caiomateus11 caio mateus <caiomateusgames@gmail.com>
2019-03-05 00:06:26 -0300

    Ajustando o player e a responsividade.

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,40 @@ theToggle.onclick = function() {
    return false;
 }
 
+var audio = document.getElementById('audio');
+var isPlaying = false;
+
+// tocar/pausar a música
+$('.btn-play-music').click(function(){
+    $(this).toggleClass('pause');
+    $(this).parent('.music-box').toggleClass('playing');
+    $(this).find('.fa').toggleClass('fa-pause');
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.play();
+    }
+});
+audio.onplaying = function() {
+  isPlaying = true;
+};
+audio.onpause = function() {
+  isPlaying = false;
+};
+
+$('.btn-sm').click(function(){
+    $(this).toggleClass('pause');
+    $(this).parent('.music-box').toggleClass('playing');
+    $(this).find('.fa').toggleClass('fa-pause');
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.play();
+    }
+});
+audio.onplaying = function() {
+  isPlaying = true;
+};
+audio.onpause = function() {
+  isPlaying = false;
+};
