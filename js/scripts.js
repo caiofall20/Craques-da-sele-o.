42dd1545a0485b1b5943bb1234a1d49b46503287
Caio Mateus <caiomateusgames@outlook.com>
2019-03-26 10:15:16 -0300

    Modificaçoes no layout.

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,87 @@
- onLoad=hidden()
-function hidden(){document.body.style.overflow='hidden';}
+// Essa função é para travar a barra de rolagem(scroll) ao carregar a página.
+// onLoad = hidden()
+ // function hidden() { document.body.style.overflow = 'hidden'; }
 
+// Essa função é para destravar a barra de rolagem(scroll) ao carregar a página.
+function LiberarBotao() {
+  document.body.style.overflow = "scroll";
+}
 
+//Função para o toggle da página
 var theToggle = document.getElementById('toggle');
-
-// based on Todd Motto functions
-// https://toddmotto.com/labs/reusable-js/
 
 // hasClass
 function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 }
 // addClass
 function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
-    }
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
+  }
 }
 // removeClass
 function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
     }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
 }
 // toggleClass
 function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
+  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
     }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  } else {
+    elem.className += ' ' + className;
+  }
 }
 
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
+theToggle.onclick = function () {
+  toggleClass(this, 'on');
+  return false;
 }
 
 var audio = document.getElementById('audio');
 var isPlaying = false;
 
 // tocar/pausar a música
-$('.btn-play-music').click(function(){
-    $(this).toggleClass('pause');
-    $(this).parent('.music-box').toggleClass('playing');
-    $(this).find('.fa').toggleClass('fa-pause');
-    if (isPlaying) {
-      audio.pause()
-    } else {
-      audio.play();
-    }
+$('.btn-play-music').click(function () {
+  $(this).toggleClass('pause');
+  $(this).parent('.music-box').toggleClass('playing');
+  $(this).find('.fa').toggleClass('fa-pause');
+  if (isPlaying) {
+    audio.pause()
+  } else {
+    audio.play();
+  }
 });
-audio.onplaying = function() {
+audio.onplaying = function () {
   isPlaying = true;
 };
-audio.onpause = function() {
+audio.onpause = function () {
   isPlaying = false;
 };
 
-$('.btn-sm').click(function(){
-    $(this).toggleClass('pause');
-    $(this).parent('.music-box').toggleClass('playing');
-    $(this).find('.fa').toggleClass('fa-pause');
-    if (isPlaying) {
-      audio.pause()
-    } else {
-      audio.play();
-    }
+$('.btn-sm').click(function () {
+  $(this).toggleClass('pause');
+  $(this).parent('.music-box').toggleClass('playing');
+  $(this).find('.fa').toggleClass('fa-pause');
+  if (isPlaying) {
+    audio.pause()
+  } else {
+    audio.play();
+  }
 });
-audio.onplaying = function() {
+audio.onplaying = function () {
   isPlaying = true;
 };
-audio.onpause = function() {
+audio.onpause = function () {
   isPlaying = false;
 };
